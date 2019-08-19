b49c4722da4c09e04be08cb7cc025e938aeb2afe
D. Czysz <=>
2019-08-19 16:57:26 -0600

    Completed 1-Intro and Setup

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,10 @@
+// import React from 'react'; // Next.js takes care of importing React
+import Link from 'next/link';
+
+const Home = props => (
+  <div>
+    <p>Home!</p>
+  </div>
+);
+
+export default Home;
