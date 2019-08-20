026786b35fea94d6f4fe9b143431f0bcc5ae5daf
D. Czysz <=>
2019-08-19 18:21:21 -0600

    Completed 2-CSS and Styled Components

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,14 +1,25 @@
 import Link from 'next/link';
 
+import NavStyles from './styles/NavStyles';
+
 const Nav = () => (
-  <div>
-    <Link href="/sell">
-      <a>Sell!</a>
-    </Link>
+  <NavStyles>
     <Link href="/">
-      <a>Home</a>
+      <a>Items</a>
     </Link>
-  </div>
+    <Link href="/sell">
+      <a>Sell</a>
+    </Link>
+    <Link href="/signup">
+      <a>Signup</a>
+    </Link>
+    <Link href="/orders">
+      <a>Orders</a>
+    </Link>
+    <Link href="/account">
+      <a>Account</a>
+    </Link>
+  </NavStyles>
 );
 
 export default Nav;
